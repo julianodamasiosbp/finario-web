bac74c4eca35be215044887a8dc7967eb54bf1ba
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-05-22 20:32:25 -0300

    fix app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './features/public/components/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: '',
     loadChildren: () =>
       import('./features/public/public.module').then((m) => m.PublicModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
   },
   { path: '**', component: NotFoundComponent },
 ];
